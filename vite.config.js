6539060127493529d649fd71aebe53e7f34c7282
William <william.9150@gmail.com>
2023-01-08 21:55:47 +0800

    test123

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/hex-vue2022-vite0108/',
+  plugins: [vue()],
+})
